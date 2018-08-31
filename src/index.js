08ebe83e7c0d88a96feb1e7941566399e5f9c507
Tare <tare.dgg@gmail.com>
2018-08-31 12:26:30 -0400

    fixed file structure?

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+//
+// // import App from './App';
+// // import registerServiceWorker from './registerServiceWorker';
+// import './index.css';
+// import './App.css';
+// import logo from './logo.svg';
+//
+//
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to A Lean Introduction to React.js </h1>
+//         </header>
+//         <p className="App-intro">
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
