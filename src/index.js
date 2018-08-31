ae06e5c1f3b2814f2bdc6cec3539cc745a4619f7
Tare <tare.dgg@gmail.com>
2018-08-31 15:25:01 -0400

    hello react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-//
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends Component {
+
+  render(){
+    return(
+        <div>
+          <h1 className="react-title">Welcome to A Lean Introduction to React</h1>
+          <p className="react-intro">Hello React!!</p>
+        </div>
+    );
+  }
+};
+
+const root = document.getElementById("root");
+ReactDOM.render(<App/>,  root)
